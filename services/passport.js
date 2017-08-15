fdaff53ac562e83e70c3cd48c1059f6085bfc5b5
Thomas Hervey <thomasahervey@gmail.com>
2017-08-14 20:31:49 -0400

    handle auth & refactored async

--- services/passport.js
+++ services/passport.js
@@ -23,18 +23,11 @@ passport.use(
 			callbackURL: '/auth/google/callback',
 			proxy: true
 		},
-		(accessToken, refreshToken, profile, done) => {
-			User.findOne({ googleId: profile.id }).then(existingUser => {
-				if (existingUser) {
-					// login user
-					done(null, existingUser)
-				} else {
-					// create a new record
-					new User({ googleId: profile.id })
-						.save()
-						.then(user => done(null, user))
-				}
-			})
+		async (accessToken, refreshToken, profile, done) => {
+			const existingUser = await User.findOne({ googleId: profile.id })
+			if (existingUser) return done(null, existingUser)
+			const user = await new User({ googleId: profile.id }).save()
+			return done(null, user)
 		}
 	)
 )
